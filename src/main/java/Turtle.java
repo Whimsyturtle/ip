258c38bd5961a6a229884700797f8b12b9170365
Whimsyturtle <19924575+Whimsyturtle@users.noreply.github.com>
2026-01-17 19:39:27 +0800

    Add command Echo

--- src/main/java/Turtle.java
+++ src/main/java/Turtle.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Turtle {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         greet();
+        while (true) {
+            String userCommand = scanner.nextLine();
+            if (userCommand.equals("bye")) {
+                break;
+            }
+            echo(userCommand);
+        }
         bye();
     }
 
     private static void greet() {
         System.out.println("Hello! My name is Turtle.\nWhat can I do for you?\n");
+    }
+
+    private static void echo(String userCommand) {
+        System.out.println("Command: " + userCommand + "\n");
     }
 
     private static void bye() {
