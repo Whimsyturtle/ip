daa28e5dc060aaecd38eb861fe46628c75bdb0f1
Whimsyturtle <19924575+Whimsyturtle@users.noreply.github.com>
2026-01-28 00:25:53 +0800

    Change task date type to LocalDate

--- src/main/java/tasks/DeadlineTask.java
+++ src/main/java/tasks/DeadlineTask.java
@@ -1,21 +1,25 @@
 package tasks;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DeadlineTask extends Task {
 
-    protected final String deadline;
+    protected final LocalDate deadline;
 
-    public DeadlineTask(String name, String deadline) {
+    public DeadlineTask(String name, LocalDate deadline) {
         this(name, false, deadline);
     }
 
-    public DeadlineTask(String name, boolean isDone, String deadline) {
+    public DeadlineTask(String name, boolean isDone, LocalDate deadline) {
         super(name, isDone);
         this.deadline = deadline;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        return "[D]" + super.toString() + " (by: "
+                + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     @Override
